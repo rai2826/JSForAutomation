902726924cf760d393280972a79c8fdb96c56864
Gaurav Rai <rai2826@gmail.com>
2024-08-17 12:44:11 +1000

    type coercion

--- TypeConversion/typeCoercion.js
+++ TypeConversion/typeCoercion.js
@@ -4,3 +4,12 @@ let currentyear = 2024;
 
 let age = currentyear - bornyear;
 console.log(age);
+
+// 2 gets treated as String due to + operator
+console.log("23" + 2 + "11");
+
+//23 and 11 gets treated as number due to - operator
+console.log("23" - 2 - "11");
+
+//23 and 2 gets treated as number due to operator *
+console.log("23" * "2");

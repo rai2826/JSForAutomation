d38cffc9057d9413f14bd21b59f0aa33996711ff
Gaurav Rai <rai2826@gmail.com>
2024-08-17 12:55:49 +1000

    Truthy and falsy

---
+++ TruthyAndFalsy/falsy.js
@@ -0,0 +1,6 @@
+//5 falsy values : 0,'',undefined,null,NaN
+//Rest everything is truthy
+//falsy values are those if they are converted to boolean will become false
+
+let a = NaN;
+console.log(Boolean(a));
